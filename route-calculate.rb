3257369e01cf7e2d356936ce27f9c0a8c7329238
grigory.marsavin <grigory.marsavin@jetruby.com>
2021-08-09 10:54:14 +0300

    fixed more spaces

--- route-calculate.rb
+++ route-calculate.rb
@@ -6,9 +6,9 @@ require_relative 'error'
 
 class RouteCalculate
   def self.get_price(point_from, point_to, service, height, width, length, weight)
-    raise SizeError if height.to_i <= 0 || width.to_i <= 0 || length.to_i <= 0 || weight <= 0
+    raise SizeError if height.to_i <= 0 || width.to_i <= 0 || length.to_i <= 0 || weight.to_i <= 0
     api = Api.new
-    distance = api.send(service,point_from, point_to)
+    distance = api.send(service, point_from, point_to)
     Calculator.calculate(height, width, length, weight, distance)
   end 
 end
