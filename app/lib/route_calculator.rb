1281a77a1c575c90e1a11dbc2b9ba6f03aee20f2
Your Name <Grifon903@gmail.com>
2021-08-17 17:05:03 +0300

    some fixes

--- app/lib/route_calculator.rb
+++ app/lib/route_calculator.rb
@@ -5,8 +5,6 @@ require_relative 'proxy'
 require_relative 'distanceorg'
 
 module RouteCalculator
-  attr_accessor :distance
-
   def self.call(hash={})
     distance = Proxy.new.call(hash[:addr_from], hash[:addr_to], 'Distanceorg', hash[:height], hash[:width], hash[:length], hash[:weight])
     Calculator.call(hash[:height], hash[:width], hash[:length], hash[:weight], distance)
