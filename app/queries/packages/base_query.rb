ffdaca75a59572a6606642afce7b2893b901d3c2
grigory.marsavin <grigory.marsavin@jetruby.com>
2021-09-02 18:01:09 +0300

    transfer Pagination functional from Grid_query to Base_query

--- app/queries/packages/base_query.rb
+++ app/queries/packages/base_query.rb
@@ -1,7 +1,15 @@
 module Packages
   class BaseQuery < Query
+    attr_accessor :params, :per_page 
+
+    def initialize(params)
+       super 
+       @params = params
+       @per_page = params[:per_page]
+    end
+
     def base_relation 
-      @relation ||= current_user.packages
+      @relation ||= current_user.packages.page(params[:page]).per(per_page)
     end
   
     def execute 
--- app/queries/packages/grid_query.rb
+++ app/queries/packages/grid_query.rb
@@ -1,16 +1,14 @@
 module Packages
   class GridQuery < Query
-    attr_accessor :sort, :params, :per_page
+    attr_accessor :sort
 
     def initialize(params)
       super
       @sort = params[:sort] || 'created_at'
-      @params = params
-      @per_page = params[:per_page]
     end
 
     def base_relation 
-      @relation ||= Packages::BaseQuery.call(current_user: current_user).page(params[:page]).per(per_page)
+      @relation ||= Packages::BaseQuery.call(current_user: current_user)
     end
     
     def execute 
