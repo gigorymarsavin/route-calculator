3bde568173933d832e315db13246c38f604e7cb2
grigorymarsavin <88383369+grigorymarsavin@users.noreply.github.com>
2021-09-06 18:18:32 +0300

    Update package.rb

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,9 +1,4 @@
 class Package < ApplicationRecord
-  include AASM
-
-  aasm do
-  end
-  include AASM
 
   validates :name, :midname, :surname, :phone, :email, :width, :length, :height, :addr_from, :addr_to, presence: true
   belongs_to :user
