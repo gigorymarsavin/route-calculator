bd938bf55cd57feae05efabdf2226f3e336449e3
grigory.marsavin <grigory.marsavin@jetruby.com>
2021-09-10 11:32:47 +0300

    [TRELLO-9] created model Organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_093211) do
+ActiveRecord::Schema.define(version: 2021_09_10_102019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2021_09_02_093211) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "packages", force: :cascade do |t|
     t.string "name"
     t.string "surname"
@@ -56,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_093211) do
     t.float "size"
     t.integer "user_id"
     t.string "aasm_state"
+    t.integer "organisation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_093211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.integer "organisation_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
