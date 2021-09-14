d12c80a6a5afad43464642e9a95889442fee27c7
grigory.marsavin <grigory.marsavin@jetruby.com>
2021-09-14 13:13:46 +0300

    [TRELLO-10] added roles to user

--- app/queries/packages/base_query.rb
+++ app/queries/packages/base_query.rb
@@ -1,7 +1,7 @@
 module Packages
   class BaseQuery < ::BaseQuery    
     def base_relation
-      current_user.packages
+      current_user.is_admin ? Package.where(organisation_id: current_user.organisation_id) : current_user.packages
     end
   end 
 
